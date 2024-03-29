dedc0a79ab0393e18a2f60c63701329bd1ebbc2d
AmolGayakwad <158410064+AmolGayakwad@users.noreply.github.com>
2024-03-29 16:16:17 +0530

    Update hashVsmap.js

--- hashVsmap.js
+++ hashVsmap.js
@@ -13,31 +13,31 @@ function generateTestData(numRows) {
 }
 
 function testHashtable(data) {
-    console.time('Hashtable Insertion');
+    console.time('Insertion Time');
     const hashtable = {};
 
     data.forEach(({ patientID, ...attributes }) => {
         hashtable[patientID] = attributes;
     });
-    console.timeEnd('Hashtable Insertion');
+    console.timeEnd('Insertion Time');
 
 }
 
 function testMap(data) {
-    console.time('Map Insertion');
+    console.time('Insertion Time');
     const map = new Map();
 
     data.forEach(({ patientID, ...attributes }) => {
         map.set(patientID, attributes);
     });
-    console.timeEnd('Map Insertion');
+    console.timeEnd('Insertion Time');
 
 }
 
 const testData = generateTestData(5000000); 
 
-console.log("Testing Hashtable:");
+console.log("Hashtable:");
 testHashtable(testData);
 
-console.log("\nTesting Map:");
+console.log("\nMap:");
 testMap(testData);
